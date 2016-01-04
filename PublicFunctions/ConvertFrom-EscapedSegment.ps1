b17d5fe78453f9ae32d9466b9e29f4762d7e66cb
Eric Sciple <ersciple@microsoft.com>
2016-01-04 09:53:47 -0500

    Fixed segment escape functions.

--- PublicFunctions/ConvertFrom-EscapedSegment.ps1
+++ PublicFunctions/ConvertFrom-EscapedSegment.ps1
@@ -1,20 +1,9 @@
-# TODO: FIX THIS TO CALL THE SEGMENT HELPER.
-
+# TODO: Move this to be a compiled cmdlet so the SegmentHelper class can be marked "internal".
 function ConvertFrom-EscapedSegment {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
         [string]$Name)
 
-    $Name.
-        Replace('%5C', '\').
-        Replace('%2F', '/').
-        #Replace('%3A', ':').
-        #Replace('???', '*').
-        #Replace('%3F', '?').
-        #Replace('%22', '"').
-        #Replace('%3C', '<').
-        #Replace('%3E', '>').
-        #Replace('%7C', '|').
-        Replace('%25', '%') # Must be unescaped last.
+    [VstsProvider.DriveItems.SegmentHelper]::Unescape($Name)
 }
--- PublicFunctions/ConvertTo-EscapedSegment.ps1
+++ PublicFunctions/ConvertTo-EscapedSegment.ps1
@@ -1,19 +1,9 @@
-# TODO: FIX THIS TO CALL THE SEGMENT HELPER.
-function ConvertFrom-EscapedSegment {
+# TODO: Move this to be a compiled cmdlet so the SegmentHelper class can be marked "internal".
+function ConvertTo-EscapedSegment {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
         [string]$Name)
 
-    $Name.
-        Replace('%', '%25'). # Must be escaped first.
-        Replace('\', '%5C').
-        Replace('/', '%2F')#.
-        #Replace(':', '%3A').
-        #Replace('*', '???').
-        #Replace('?', '%3F').
-        #Replace('"', '%22').
-        #Replace('<', '%3C').
-        #Replace('>', '%3E').
-        #Replace('|', '%7C')
+    [VstsProvider.DriveItems.SegmentHelper]::Escape($Name)
 }
