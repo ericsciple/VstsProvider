a25d0b4cd6042adb1767e7451371a999b3ffbe60
Eric Sciple <ersciple@microsoft.com>
2016-01-06 02:11:44 -0500

    Working on restore.

--- PrivateFunctions/Restore-GitRepos.ps1
+++ PrivateFunctions/Restore-GitRepos.ps1
@@ -20,6 +20,7 @@ function Restore-GitRepos {
         Where-Object { $_ -is [System.IO.DirectoryInfo] }
     foreach ($repoDirectory in $repoDirectories) {
         $repoSegment = $repoDirectory.Name
+        $repoVstsPath = "$reposVstsPath\$repoSegment"
         $repoName = ConvertFrom-EscapedSegment $repoSegment
         $repo = $repos[$repoDirectory.Name]
         if (!$repo) {
@@ -33,6 +34,45 @@ function Restore-GitRepos {
             Write-Verbose "Repo exists: $repoName"
         }
 
-        #Restore-Repos -ProjectVstsPath $projectVstsPath -ProjectDirectory $projectDirectory.FullName
+        # Get the refs.
+        $refs = @{ }
+        $refsVstsPath = "$repoVstsPath\Refs"
+        Write-Verbose "Getting refs: $refsVstsPath"
+        Get-ChildItem -LiteralPath $refsVstsPath |
+            ForEach-Object { $refs[$_.PSVstsChildName] = $_ }
+
+        # Get the ref directories.
+        $refDirectories = Get-ChildItem -LiteralPath "$($repoDirectory.FullName)\Refs" -ErrorAction Ignore |
+            Where-Object { $_ -is [System.IO.DirectoryInfo] }
+        foreach ($refDirectory in $refDirectories) {
+            $refSegment = $refDirectory.Name
+            $refVstsPath = "$refsVstsPath\$refSegment"
+            $refName = ConvertFrom-EscapedSegment $refSegment
+            if ($refName -notlike 'heads/*') {
+                continue
+            }
+
+            # Get the blob items.
+            $blobItems = @{ }
+            if ($refs[$refSegment]) {
+                $itemsVstsPath = "$refVstsPath\Items"
+                Write-Verbose "Getting items: $itemsVstsPath"
+                Get-ChildItem -LiteralPath $itemsVstsPath |
+                    Where-Object { $_.GitObjectType -eq 'Blob' } |
+                    ForEach-Object { $blobItems[$_.Path] = $_ }
+            }
+
+            # Get the file items.
+            $files = Get-ChildItem -LiteralPath "$($refDirectory.FullName)\Items" -Recurse |
+                Where-Object { $_ -is [System.IO.FileInfo] }
+            foreach ($file in $files) {
+                $gitPath = $file.FullName.Substring("$($refDirectory.FullName)\Items".Length).Replace('\', '/')
+                if ($blobItems[$gitPath]) {
+                    Write-Debug "Exists: $gitPath"
+                } else {
+                    #Write-Verbose "Not exists: $gitPath"
+                }
+            }
+        }
     }
 }
